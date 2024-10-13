65055514a4252c71bb5222eba751d90fad64829e
Darkheartreturns <darkheartreturns2019@gmail.com>
2024-10-13 23:29:32 +0300

    Delete

--- person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, username, user_id, password):
-        self.username = username
-        self.user_id = user_id
-        self.password = password
-
-    def get_info(self):
-        return f"Username: {self.username}, User ID: {self.user_id}"
--- userGUI.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-class User_Gui(Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Library Management System / User")
